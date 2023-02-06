fd222c899bdfd529bc42f9c3ec35cdefe48b98b6
Alen1407 <114245751+Alen1407@users.noreply.github.com>
2023-02-06 15:38:17 +0400

    Add files via upload

---
+++ BitVector.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class BitVector:
+    def __init__(self, size):
+        self.__data = np.zeros((size // 64 + 1), dtype=np.uint)
+
+    def set(self, index:int):
+            byte_index = index // 64
+            bit_index = index % 64
+            self.__data[byte_index] = self.__data[byte_index] | np.uint(1 << bit_index)
+
+    def reset(self, index:int):
+        byte_index = index // 64
+        bit_index = index % 64
+        self.__data[byte_index] &= np.uint(~(1 << bit_index))
+
+
+    def print(self):
+        print(self.__data)
+
+bv = BitVector(300)
+bv.set(0)
+bv.set(2)
+bv.set(3)
+bv.set(4)
+bv.set(4)
+bv.print()
